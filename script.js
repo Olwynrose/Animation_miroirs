0a3e7f98d80e988c0be1435573fb0cf53211cdd5
Olwynrose <lauriane.elena.fels@gmail.com>
2018-04-24 15:58:30 +0200

    Second commit
    Adaptation en temps réel de toutes les options OK
    Affichage des rayons incidents OK

--- script.js
+++ script.js
@@ -24,26 +24,47 @@ window.onload = function()
   document.getElementById("input_nbmir").oninput = function(){
     nbmir = document.getElementById("input_nbmir").value;
     document.getElementById("val_nbmir").innerHTML = nbmir;
-  }
-  document.getElementById("input_nbmir").onchange = function(){
     context.clearRect(0, 0, canvas.width, canvas.height);
     Affichage(nbmir, F, l, d, theta, sun, e, echelle);
   }
   document.getElementById("input_theta").oninput = function(){
     theta = document.getElementById("input_theta").value;
     document.getElementById("val_theta").innerHTML = theta + " &deg;";
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    Affichage(nbmir, F, l, d, theta, sun, e, echelle);
   }
   document.getElementById("input_F").oninput = function(){
     F = document.getElementById("input_F").value;
     document.getElementById("val_F").innerHTML = F/100 + " m";
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    Affichage(nbmir, F, l, d, theta, sun, e, echelle);
   }
   document.getElementById("input_l").oninput = function(){
     l = document.getElementById("input_l").value;
     document.getElementById("val_l").innerHTML = l + " cm";
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    Affichage(nbmir, F, l, d, theta, sun, e, echelle);
   }
   document.getElementById("input_d").oninput = function(){
     d = document.getElementById("input_d").value;
     document.getElementById("val_d").innerHTML = d + " cm";
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    Affichage(nbmir, F, l, d, theta, sun, e, echelle);
+  }
+
+
+
+  document.getElementById("input_etal").onchange = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    Affichage(nbmir, F, l, d, theta, sun, e, echelle);
+  }
+  document.getElementById("input_arc").onchange = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    Affichage(nbmir, F, l, d, theta, sun, e, echelle);
+  }
+  document.getElementById("input_ray").onchange = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    Affichage(nbmir, F, l, d, theta, sun, e, echelle);
   }
 
   //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -59,6 +80,7 @@ window.onload = function()
   }
 
   var context = canvas.getContext('2d');
+  var context1 = canvas.getContext('2d');
   if(!context)
   {
     alert("Impossible de récupérer le context du canvas");
@@ -85,6 +107,11 @@ window.onload = function()
 
 
     context.beginPath();//On démarre un nouveau tracé
+    context1.beginPath();
+
+    var xi;
+    var yi;
+    var xa;
 
     var i = (nbmir - 1) /2;
     for (var pos = -i ; pos <= i ; pos++)
@@ -94,12 +121,26 @@ window.onload = function()
       y2 = Math.abs(l * Math.tan(alpha));
 
       if (alpha >= 0) {
-        context.moveTo((jdim/2) + L-(l/2), idim - 30);//On se déplace au coin inférieur gauche
+
+        /* DESSIN DU MIROIR */
+        context.moveTo((jdim/2) + L-(l/2), idim - 30);
         context.lineTo((jdim/2) + L+(l/2), idim - 30);
         context.lineTo((jdim/2) + L+(l/2), idim - 30 - y2 - e);
         context.lineTo((jdim/2) + L-(l/2), idim - 30 - e);
         context.lineTo((jdim/2) + L-(l/2), idim - 30);
-        context.stroke();//On trace seulement les lignes.
+        context.stroke();
+
+        /* DESSIN DU RAYON INCIDENT */
+        if ((document.getElementById("input_ray")).checked == true)
+        {
+          xi = L;
+          yi = e + (y2/2);
+          xa = L + Math.tan(theta) * (sun - yi);
+
+          context1.moveTo((jdim/2) + xa, idim - 30 - sun);
+          context1.lineTo((jdim/2) + xi, idim - 30 - yi);
+        }
+
       }
       else {
         context.moveTo((jdim/2) + L-(l/2), idim - 30);//On se déplace au coin inférieur gauche
@@ -108,6 +149,18 @@ window.onload = function()
         context.lineTo((jdim/2) + L-(l/2), idim - 30 - y2 - e);
         context.lineTo((jdim/2) + L-(l/2), idim - 30);
         context.stroke();//On trace seulement les lignes.
+
+        /* DESSIN DU RAYON INCIDENT */
+        if ((document.getElementById("input_ray")).checked == true)
+        {
+          xi = L;
+          yi = e + (y2/2);
+          xa = L + Math.tan(theta) * (sun - yi);
+
+          context1.moveTo((jdim/2) + xa, idim - 30 - sun);
+          context1.lineTo((jdim/2) + xi, idim - 30 - yi);
+        }
+
       }
 
     }
